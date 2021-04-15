cc74e4328e7e6e506454ce22bfffe42b8e457e2d
niknefed <niknefed.github.com>
2021-04-16 01:21:40 +0300

    swsu hostel

---
+++ getRooms.py
@@ -0,0 +1,58 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlTableModel
+from lodgers import Ui_lodgers
+
+
+# новый класс
+class Lodgers(QtWidgets.QMainWindow, Ui_lodgers):
+    def __init__(self, parent=None):
+        super(Lodgers, self).__init__(parent)
+        self.setupUi(self)
+
+        sdb = QSqlDatabase.addDatabase('QSQLITE')
+        sdb.setDatabaseName('baza.db')
+        sdb.open()
+        self.model = QSqlTableModel(db=sdb)
+        self.model.setTable("users")
+        self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+        self.model.select()
+
+        self.tableView_1.setModel(self.model)
+
+        self.rоом_beds = []
+
+
+        self.pushButton_9.clicked.connect(self.on_add_record)  # Добавить запись
+        self.pushButton_10.clicked.connect(self.on_del_record)  # Удалить запись
+
+    def on_add_record(self):
+        row = self.tableView_1.selectionModel().currentIndex().row()
+        if self.tableView_1.selectionModel().currentIndex().row() == -1:
+            row = 0
+            self.model.insertRow(row)
+        else:
+            row = row + 1
+            self.model.insertRow(row)
+
+        self.model.setData(self.model.index(row, 0), 'ФИО NEW')
+        self.model.setData(self.model.index(row, 1), '0000')
+        self.model.setData(self.model.index(row, 2), '0')
+        self.model.setData(self.model.index(row, 3), '0')
+        self.model.submit()
+
+    def on_del_record(self):
+        self.model.removeRow(self.tableView_1.currentIndex().row())
+        self.model.select()
+
+
+
+    def _getRooms(self):
+        self.rоом_beds = []
+        for row in range(self.model.rowCount()):
+            if self.model.data(self.model.index(row, 2)) == 1:
+                rоом_bed = str(self.model.data(
+                    self.model.index(row, 1)))[:3], \
+                           str(self.model.data(self.model.index(row, 1)))[3]
+                self.rоом_beds.append(rоом_bed)
+
---
+++ main .py
@@ -0,0 +1,122 @@
+import sys
+from mainWindow import Ui_MainWindow
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from getRooms import Lodgers
+
+
+class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.scene = QtWidgets.QGraphicsScene()
+        self.setupUi(self)
+        self.lodgers = Lodgers()
+        self.lodgers.pushButton_8.clicked.connect(self.returnMainWindow1)
+
+        self.rоом_beds = []
+        self.rоом_rect = {
+            '131': 0,
+            '132': 1,
+            '133': 2,
+            '134': 3,
+            '135': 4,
+            '136': 5,
+            '137': 6,
+            '138': 7,
+        }
+
+
+        self.pushButton_7.clicked.connect(self.openlodgers)
+        # Функция закрытия главного окна по кнопке Выход
+        self.pushButton_4.clicked.connect(self.close)
+        self.scene.addPixmap(QtGui.QPixmap("section_sketch.png"))
+        self.pushButton_8.clicked.connect(self.drowRooms)
+
+        self.graphicsView.setScene(self.scene)
+
+
+    # Метод для отрисовки
+    def drowRooms(self):
+        if not self.lodgers.rоом_beds:
+            msg = QtWidgets.QMessageBox.information(
+                self,
+                'Message',
+                f'Просматривать рано. \n'
+                f'Нажмите кнопку `Проживающие` и выполните необходимые корректировки.'
+            )
+            return
+
+        self.Rooms = []
+        self.Rooms.append((
+            # Рисование прямоугольников для 131 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя через список)
+            self.scene.addRect(195, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1311
+            self.scene.addRect(240, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1312
+            self.scene.addRect(195, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1313
+            self.scene.addRect(240, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1314
+        self.Rooms.append((
+            # Рисование прямоугольников для 132 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(330, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1321
+            self.scene.addRect(375, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1322
+            self.scene.addRect(330, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1323
+            self.scene.addRect(375, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1324
+        self.Rooms.append((
+            # Рисование прямоугольников для 133 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(465, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1331
+            self.scene.addRect(510, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1332
+            self.scene.addRect(465, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1333
+            self.scene.addRect(510, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1334
+        self.Rooms.append((
+            # Рисование прямоугольников для 134 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(595, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1341
+            self.scene.addRect(640, 40, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1342
+            self.scene.addRect(595, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1343
+            self.scene.addRect(640, 85, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1344
+        self.Rooms.append((
+            # Рисование прямоугольников для 135 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(595, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1351
+            self.scene.addRect(640, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1352
+            self.scene.addRect(595, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1353
+            self.scene.addRect(640, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1354
+        self.Rooms.append((
+            # Рисование прямоугольников для 136 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(465, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1361
+            self.scene.addRect(510, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1362
+            self.scene.addRect(465, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1363
+            self.scene.addRect(510, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1364
+        self.Rooms.append((
+            # Рисование прямоугольников для 137 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(330, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1371
+            self.scene.addRect(375, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1372
+            self.scene.addRect(330, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1373
+            self.scene.addRect(375, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1374
+        self.Rooms.append((
+            # Рисование прямоугольников для 138 комнаты (обозначения для того чтобы в дальнеёшем присвоить прямоугольникам имя)
+            self.scene.addRect(195, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1381
+            self.scene.addRect(240, 320, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1382
+            self.scene.addRect(195, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green)),  # 1383
+            self.scene.addRect(240, 365, 35, 35, pen=QtGui.QPen(Qt.black), brush=QtGui.QBrush(Qt.green))))  # 1384
+
+
+        # список сформированный в новом классе
+        for k, v in self.lodgers.rоом_beds:
+            self.Rooms[self.rоом_rect[k]][int(v)].setBrush(QtGui.QBrush(Qt.red))
+
+
+    def openlodgers(self):
+        self.hide()
+        self.lodgers.show()
+
+
+    # Функция для возвращения на главное окно (Из окна проживающие) по кнопке Назад
+    def returnMainWindow1(self):
+        self.lodgers._getRooms()  # формирование списка self.lodgers.rоом_beds
+        self.drowRooms()  # отрисовка
+        self.lodgers.close()
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    sys.exit(app.exec_())
